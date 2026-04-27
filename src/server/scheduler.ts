053f38588df33b2bffc5428aa8b7ca1003040bfa
Adrian Dabek <adrian950113@gmail.com>
2026-04-27 16:10:19 +0200

    Remove hearthbeat cron

--- src/server/scheduler.ts
+++ src/server/scheduler.ts
@@ -27,19 +27,9 @@ export function startScheduler(): void {
 
   log("init", "Scheduler starting");
 
-  // Run once immediately so we know it works after deploy/restart
-  runCleanup();
-
   cron.schedule("0 4 * * *", () => {
     runCleanup();
   });
 
   log("init", "Scheduled daily cleanup at 04:00 UTC");
-
-  // Heartbeat: log every minute to verify scheduler is running
-  cron.schedule("* * * * *", () => {
-    log("heartbeat", "Scheduler alive - tick every minute");
-  });
-
-  log("init", "Scheduled heartbeat every minute");
 }
