5bc877e3805ec4f7dfe7ebdb41db6c0ec894ff79
Adrian Dabek <adrian950113@gmail.com>
2026-04-29 11:37:15 +0200

    Add custom icons and name

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -10,11 +10,16 @@ export const RouterHead = component$(() => {
 
   return (
     <>
-      <title>{head.title}</title>
+      <title>{head.title ? `Bombos 2.5 - ${head.title}` : 'Bombos 2.5'}</title>
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="shortcut icon" href="/icons/favicon.ico" />
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      <link rel="manifest" href="/manifest.json" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
