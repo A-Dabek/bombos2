4dec650908ed26302a8b8ddbf2555fbff92f27be
Adrian Dabek <adrian950113@gmail.com>
2026-04-29 09:49:28 +0200

    Add BackButton shared component

---
+++ src/components/shared/BackButton.tsx
@@ -0,0 +1,21 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import { HiArrowLeftOutline } from "@qwikest/icons/heroicons";
+
+interface BackButtonProps {
+  href: string;
+}
+
+export default component$<BackButtonProps>(({ href }) => {
+  return (
+    <div class="flex items-center mb-4">
+      <Link
+        href={href}
+        class="flex items-center text-gray-500 hover:text-gray-700"
+      >
+        <HiArrowLeftOutline class="w-5 h-5 mr-1" />
+        <span>Back</span>
+      </Link>
+    </div>
+  );
+});
