7a1da0f9c5698624892709a8efc7ffdf30314890
Adrian Dabek <adrian950113@gmail.com>
2026-06-08 15:43:27 +0200

    Add automatic backup

--- prod.sh
+++ prod.sh
@@ -9,6 +9,9 @@ export NODE_ENV=production
 # Optional: Define a custom port (default is 3000)
 export PORT=3000
 
+# Backup DB with timestamp (YYYYMMDD-HHMM)
+cp data/app.db "backup/app-$(date +%Y%m%d-%H%M).db"
+
 pnpm build || true
 
 # For Fastify
